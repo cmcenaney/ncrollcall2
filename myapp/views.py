2d38e7122af9d3cf34113711706cb98790656fa7
Colleen McEnaney <colleenmcenaney@gmail.com>
2014-03-05 16:05:32 -0500

    fixed bill urls and views

--- myapp/views.py
+++ myapp/views.py
@@ -18,19 +18,18 @@ def bill(request):
 	context = {'bill': bill}
 	return render(request,'bills.html',context)
 
+def each_bill(request,bill_id):
+	each_bill = get_object_or_404(gadb_bill, bill_id=bill_id)
+	return render(request, "eachbill.html", {'each_bill': each_bill})
+
 def member(request):
 	member = gadb_legislator.objects.all()
 	context = {'member': member}
 	return render (request, 'members.html',context)
 
-def each_member(request,pk):
-	each_member = get_object_or_404(gadb_legislator, legislator_id=pk)
+def each_member(request,legislator_id):
+	each_member = get_object_or_404(gadb_legislator, legislator_id=legislator_id)
 	return render(request, "eachmember.html", {'each_member': each_member})
-
-
-	# each_member = gadb_legislator.objects.all()
-	# context = {'each_member': each_member}
-	# return render (request, 'eachmember.html',context)
 
 def gadb_stats(request):
 	return HttpResponse("votes")
--- project/urls.py
+++ project/urls.py
@@ -6,16 +6,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^home/', 'myapp.views.home', name='home'),
-    url(r'^bills/', 'myapp.views.bill', name='gadb_bill'),
+    url(r'^bills/$', 'myapp.views.bill', name='gadb_bill'),
     url(r'^members/$', 'myapp.views.member', name='gadb_legislator'),
 
-
+   	url(r'^members/(?P<legislator_id>\d+)$', 'myapp.views.each_member', name='each_member'),
+    url(r'^bills/(?P<bill_id>\d+)/$', 'myapp.views.each_bill', name='each_bill'),
     
-   	url(r'^members/(?P<pk>\d+)$', 'myapp.views.each_member', name='each_member'),
-    url(r'^bills/(?P<bill_id>\d+)/$', 'myapp.views.bill', name='gadb_bill'),
-
-
-
     url(r'^$', 'myapp.views.index', name='index'),
     url(r'^$', include('myapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
